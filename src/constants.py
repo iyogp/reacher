eab52ce5e7522aa9ef5974807ced8b891f4ea307
iyogp <patrickiyogdev@gmail.com>
2024-07-20 16:39:34 +1000

    create constants file

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+# environment variable name for the URL of the UTR server
+UTR_URL = "UTR_URL"
+
+# environment variable name for the username of a UTR account
+UTR_USERNAME = "UTR_USERNAME"
+
+# environment variable name for the password of a UTR account
+UTR_PASSWORD = "UTR_PASSWORD"
+
+# utr account user id
+UTR_USER_ID = "UTR_USER_ID"
