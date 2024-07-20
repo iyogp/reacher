dcc4b56bc0453537edc01a6d9192bced9d3c2b65
iyogp <patrickiyogdev@gmail.com>
2024-07-20 14:04:09 +1000

    add exceptions for parsing arguments and environment variables

--- src/exceptions/exceptions.py
+++ src/exceptions/exceptions.py
@@ -1,6 +1,16 @@
 from requests import HTTPError
 
 
+class ArgumentInputNotParsedError(ValueError):
+    """Raised when an argument is not parsed correctly."""
+    pass
+
+
+class EnvironmentInputNotParsedError(ValueError):
+    """Raised when an environment is not parsed correctly."""
+    pass
+
+
 class CannotExtractUtrData(HTTPError):
     """Exception raised when a request fails to extract Utr data."""
     pass
